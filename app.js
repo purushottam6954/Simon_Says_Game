a8bdf11264057e7240344f3c14c0efb498dd5642
purushottam6954 <purushottamkumar6955@gmail.com>
2025-09-01 17:04:47 +0530

    MatchingSequence

--- app.js
+++ app.js
@@ -9,16 +9,15 @@ document.addEventListener("keypress",function(){
     if(started==false){
         console.log("Game is Started");
         started=true;
-
       levelUp();
     }
 })
 
-function btnFlash(btn){
+function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
-    },500);
+    },700);
 }
 
 function levelUp(){
@@ -29,5 +28,48 @@ function levelUp(){
     let randInt=Math.floor(Math.random()*3);
     let randColor=colors[randInt];
     let randBtn=document.querySelector(`.${randColor}`);
-    btnFlash(randBtn);
+    gameFlash(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },1000)
+}
+
+function checkAns(){
+    let idx=level-1;
+    if(userSeq[idx]==gameSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+            setTimeout(levelUp(),1000);
+        }
+        console.log("Same value");
+    }else{
+        let body=document.querySelector("body");
+        body.classList.add("gameOver");
+        h2.innerText="Game Over!Press any key to Start";
+        setTimeout(function(){
+            body.classList.remove("gameOver");
+        },400);
+    }
+}
+
+function btnPress(){
+    // console.log(this);
+    let btn=this;
+   userFlash(btn);
+   let userColor=btn.getAttribute("id");
+   console.log(userColor);
+   userSeq.push(userColor);
+   console.log(userSeq);
+
+   checkAns();
+}
+
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
 }
